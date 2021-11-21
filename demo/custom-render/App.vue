2e617f95667a25937f40e9f8d52fa9886017dd6f
Sander Moolin <sandermoolin@gmail.com>
2021-11-21 09:09:36 -0500

    renderer override in place

--- demo/custom-render/App.vue
+++ demo/custom-render/App.vue
@@ -1,21 +1,64 @@
 <template>
-    <Lunchbox :cameraPosition="[0, 0, 5]">
-        <mesh @click="sceneIndex = 1">
+    <Lunchbox :cameraPosition="[-5, 2, 5]">
+        <mesh @click="toggleEffects">
             <boxGeometry />
             <meshBasicMaterial color="blue" />
         </mesh>
 
-        <scene v-if="sceneIndex === 1">
-            <mesh @click="sceneIndex = 0">
-                <sphereGeometry />
-                <meshBasicMaterial color="green" />
-            </mesh>
-        </scene>
+        <OrbitControlsWrapper />
     </Lunchbox>
 </template>
 
 <script lang="ts" setup>
-import { ref, watch } from 'vue'
+import { clearCustomRender, setCustomRender } from '../../src'
+import OrbitControlsWrapper from '../../extras/OrbitControlsWrapper.vue'
+import { Vector2 } from 'three'
 
-const sceneIndex = ref(0)
+// effect composer
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass'
+
+let composer: EffectComposer | null = null
+
+// function to add fx to scene
+const addFx = () =>
+    setCustomRender((opts) => {
+        const canvas = opts.renderer?.domElement
+
+        // ignore if no canvas
+        if (!canvas || !opts.scene || !opts.camera) return
+
+        // setup effect composer if needed
+        if (!composer) {
+            composer = new EffectComposer(opts.renderer as THREE.WebGLRenderer)
+
+            const renderPass = new RenderPass(opts.scene, opts.camera)
+            composer.addPass(renderPass)
+
+            const bloomPass = new UnrealBloomPass(
+                new Vector2(canvas.width, canvas.height),
+                1,
+                0.5,
+                0
+            )
+            composer.addPass(bloomPass)
+        }
+
+        composer.render()
+    })
+
+addFx()
+
+// toggle fx on and off
+let fxOn = true
+const toggleEffects = () => {
+    if (fxOn) {
+        clearCustomRender()
+    } else {
+        addFx()
+    }
+
+    fxOn = !fxOn
+}
 </script>
