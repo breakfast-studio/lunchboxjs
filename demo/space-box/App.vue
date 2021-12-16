e96e74e319787be17c493e7f956e7ee456dffdec
Sander Moolin <sandermoolin@gmail.com>
2021-12-16 08:55:00 -0500

    setup

---
+++ demo/space-box/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <Lunchbox transparent :cameraPosition="[5, 5, 5]" :cameraLook="[0, 0, 0]">
+        <Container />
+    </Lunchbox>
+</template>
+
+<script lang="ts" setup>
+import Container from './components/Container.vue'
+</script>
---
+++ demo/space-box/components/Container.vue
@@ -0,0 +1,13 @@
+<template>
+    <mesh :scale="5">
+        <boxGeometry />
+        <!-- TODO: parallaxed uv, space material -->
+        <meshStandardMaterial color="blue" :side="BackSide" />
+
+        <pointLight />
+    </mesh>
+</template>
+
+<script lang="ts" setup>
+import { BackSide } from 'three'
+</script>
