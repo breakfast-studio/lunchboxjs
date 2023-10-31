1778b6a4ff1852d1ae5a6541792db07c4da0fcc4
Sander <sandermoolin@gmail.com>
2023-10-31 11:00:33 -0400

    formatting

--- packages/lunchboxjs/utils/vite.config.ts
+++ packages/lunchboxjs/utils/vite.config.ts
@@ -1,32 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import glsl from 'vite-plugin-glsl'
-// import demos from '../demo/home/demos.json'
-import camelCase from 'lodash/camelCase'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-
-// dynamically add demo urls to build
-// const demoPages = demos.reduce((acc, curr) => {
-//     acc[curr.title] = `${
-//         (
-//             curr.url?.replace(/\/$/, '') || 'demo/' + camelCase(curr.title)
-//         ).replace(/^\//g, '') + '/index.html'
-//     }`
-//     return acc
-// }, {} as Record<string, string>)
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import glsl from "vite-plugin-glsl";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), glsl(), vueJsx()],
-    build: {
-        rollupOptions: {
-            input: {
-                main: 'index.html',
-
-                // ...demoPages,
-            },
-        },
-        outDir: 'dist',
-        emptyOutDir: true,
+  plugins: [vue(), glsl(), vueJsx()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+      },
     },
-})
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+});
