3897ff89f74335bdd2494010d7e0adcafd55157f
Sander <sander@doxel.ai>
2024-04-29 16:54:10 -0400

    snapshot

--- v0.3/vite.config.ts
+++ v0.3/vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
-import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [dts({
-    rollupTypes: true,
-  }), vue()],
+  plugins: [vue()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/lunchboxjs/index.ts'),
