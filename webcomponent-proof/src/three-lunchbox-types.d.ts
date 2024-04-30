948721914f7bfc9c0f433d390dc54b9a3cf6d330
Sander <sander@doxel.ai>
2024-04-30 11:23:34 -0400

    snapshot

---
+++ webcomponent-proof/src/three-lunchbox-types.d.ts
@@ -0,0 +1,5 @@
+declare global {
+    interface HTMLElementTagNameMap {
+        'three-lunchbox': ThreeLunchbox
+    }
+}
--- webcomponent-proof/src/three-lunchbox.ts
+++ webcomponent-proof/src/three-lunchbox.ts
@@ -8,16 +8,44 @@ export class ThreeLunchbox extends LitElement {
   render() {
     return html`
       <slot name="renderer">
-        Renderer
+        <web-gl-renderer/>
       </slot>
-      <slot name="camera"></slot>
-      <slot name="scene"></slot>
+      <slot name="camera">
+        <perspective-camera/>
+      </slot>
+      <slot name="scene">
+        <three-scene>
+          <slot></slot>
+        </three-scene>
+      </slot>
     `
   }
 }
 
-declare global {
-  interface HTMLElementTagNameMap {
-    'three-lunchbox': ThreeLunchbox
+
+
+// Programmatically-generated elements
+import * as THREE from 'three';
+
+const autoComponents: Partial<keyof typeof THREE>[] = [
+  'WebGLRenderer',
+  'PerspectiveCamera',
+  'Scene',
+]
+
+autoComponents.forEach(className => {
+  // convert name to kebab-case; prepend `three-` if needed
+  let kebabCase = className.split(/\.?(?=[A-Z])/).join('-').toLowerCase().replace(/-g-l-/, '-gl-');
+  if (!kebabCase.includes('-')) {
+    kebabCase = `three-${kebabCase}`
   }
-}
+
+  customElements.define(kebabCase, class extends LitElement {
+    render() {
+      return html`
+      <h1>${className}!</h1>
+      <slot></slot>
+      `
+    }
+  })
+})
