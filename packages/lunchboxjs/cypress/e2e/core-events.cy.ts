d7db74249d7e59d8260ab37e86ef02c92a366998
Sander <sander@doxel.ai>
2024-05-10 15:33:37 -0400

    better click test

--- packages/lunchboxjs/cypress/e2e/core-events.cy.ts
+++ packages/lunchboxjs/cypress/e2e/core-events.cy.ts
@@ -1,6 +1,4 @@
-import { Lunchbox, ThreeIntersectEvent, ThreeLunchbox } from "../../src";
-import type * as THREE from 'three';
-import { Vector3 } from "three";
+import { ThreeIntersectEvent } from "../../src";
 
 describe('vanilla HTML spec', () => {
   beforeEach(() => {
@@ -23,13 +21,17 @@ describe('vanilla HTML spec', () => {
   });
 
   it('fires the custom click event correctly', () => {
-    cy.get('three-mesh[data-name="base"]').then(async c => {
-      // add threeclick listener as custom value
-      c.get(0).addEventListener('threeclick', (evt) => {
-        const event = evt as CustomEvent<ThreeIntersectEvent>;
-        expect(event.detail.element?.getAttribute('data-name')).to.eq('base');
-        expect(event.detail.intersect.point.toArray()).to.deep.eq([0, 0, -4.0205678939819345]);
+    cy.window().then(win => {
+      cy.get('three-mesh[data-name="base"]').then(async c => {
+        // add threeclick listener as custom value
+        c.get(0).addEventListener('threeclick', (evt) => {
+          const event = evt as CustomEvent<ThreeIntersectEvent>;
+          win.console.log(event.detail.element?.getAttribute('data-name'));
+          expect(event.detail.intersect.point.toArray()).to.deep.eq([0, 0, -4.0205678939819345]);
+        });
       });
     });
+    cy.get('three-lunchbox').trigger('click', 'center');
+    cy.get('@consoleLog').should('be.calledWith', 'base');
   });
 });
