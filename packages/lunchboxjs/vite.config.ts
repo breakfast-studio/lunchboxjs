1ef4bf5ae865643f032fd19b0c2a824262197143
Sander <sandermoolin@gmail.com>
2026-01-15 12:25:26 -0500

    fix type exports

--- packages/lunchboxjs/vite.config.ts
+++ packages/lunchboxjs/vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [dts({
-    })],
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
